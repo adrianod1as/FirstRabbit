b108951517b7860d832140991b3c315d9ebafe04
Adriano Dias <adrianodiasx93@gmail.com>
2016-05-08 19:07:25 -0300

    simples envio de mensagens via rabbitmq

---
+++ FirstRabbit/app/src/main/java/com/sd/firstrabbit/ActivityHome.java
@@ -0,0 +1,234 @@
+package com.sd.firstrabbit;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import com.sd.firstrabbit.R;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.AMQP.Queue.DeclareOk;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ActivityHome extends Activity {
+    private String contact;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Spinner element
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("adrianodiasx93");
+        categories.add("ericmoura");
+        categories.add("alexpud");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        contact = "adrianodiasx93";
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                contact = spinner.getItemAtPosition(position).toString();
+                Toast.makeText(ActivityHome.this, "Selected: " + contact, Toast.LENGTH_LONG).show();
+                publishToAMQP(contact);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+
+
+
+
+
+
+
+        setupConnectionFactory();
+        publishToAMQP(contact);
+        setupPubButton();
+
+        final Handler incomingMessageHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String message = msg.getData().getString("msg");
+                TextView tv = (TextView) findViewById(R.id.textView);
+                Date now = new Date();
+                SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
+                tv.append(ft.format(now) + ' ' + message + '\n');
+            }
+        };
+        subscribe(incomingMessageHandler);
+    }
+
+
+
+
+    void setupPubButton() {
+        Button button = (Button) findViewById(R.id.publish);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                EditText et = (EditText) findViewById(R.id.text);
+                publishMessage(et.getText().toString());
+                et.setText("");
+            }
+        });
+    }
+
+    Thread subscribeThread;
+    Thread publishThread;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        publishThread.interrupt();
+        subscribeThread.interrupt();
+    }
+
+    private BlockingDeque<String> queue = new LinkedBlockingDeque<String>();
+    void publishMessage(String message) {
+        //Adds a message to internal blocking queue
+        try {
+            Log.d("","[q] " + message);
+            queue.putLast(message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ConnectionFactory factory = new ConnectionFactory();
+    private void setupConnectionFactory() {
+        factory.setAutomaticRecoveryEnabled(false);
+        factory.setHost("Alexpud.koding.io");
+        /*String uri = "Alexpud.koding.io";
+        try {
+            factory.setUri(uri);
+        } catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
+            e1.printStackTrace();
+        }*/
+    }
+
+    void subscribe(final Handler handler)
+    {
+        subscribeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Connection connection = factory.newConnection();
+                        Channel channel = connection.createChannel();
+                        channel.basicQos(1);
+                        DeclareOk q = channel.queueDeclare("ericmoura", false, false, false, null);
+                        channel.queueBind(q.getQueue(), "amq.direct", "ericmoura");
+                        QueueingConsumer consumer = new QueueingConsumer(channel);
+                        channel.basicConsume(q.getQueue(), true, consumer);
+
+                        // Process deliveries
+                        while (true) {
+                            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+                            String message = new String(delivery.getBody());
+                            Log.d("","[r] " + message);
+
+                            Message msg = handler.obtainMessage();
+                            Bundle bundle = new Bundle();
+
+                            bundle.putString("msg", message);
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (Exception e1) {
+                        Log.d("", "Connection broken: " + e1.getClass().getName());
+                        try {
+                            Thread.sleep(4000); //sleep and then try again
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        subscribeThread.start();
+    }
+
+    public void publishToAMQP(final String contact)
+    {
+        publishThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Connection connection = factory.newConnection();
+                        Channel ch = connection.createChannel();
+                        ch.confirmSelect();
+
+                        while (true) {
+                            String message = queue.takeFirst();
+                            try{
+                                ch.queueDeclare(contact, false, false, false, null);
+                                ch.basicPublish("amq.direct", contact, null, message.getBytes());
+                                Log.d("", "[s] " + message);
+                                ch.waitForConfirmsOrDie();
+                            } catch (Exception e){
+                                Log.d("","[f] " + message);
+                                queue.putFirst(message);
+                                throw e;
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (Exception e) {
+                        Log.d("", "Connection broken: " + e.getClass().getName());
+                        try {
+                            Thread.sleep(5000); //sleep and then try again
+                        } catch (InterruptedException e1) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        publishThread.start();
+    }
+}
